a0f931478feb98bc25632e04a43c6072c2ae44d9
MattBCM <matbcm01@gmail.com>
2022-03-21 18:18:46 -0400

    created main page

---
+++ script.js
@@ -0,0 +1,24 @@
+function unBlur(){
+    var img = document.querySelector(".right");
+    img.classList.add("noBlur");
+    console.log("class added");
+}
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
+
+function backHome(){
+    window.location.href="index.html";
+}
